c9b83985437aa463e3e863c6c0b4538114a8af2f
DanyKuzmenko <sh0k555@yandex.ru>
2021-10-11 21:16:05 +0300

    fix: исправить ошибки js, добавить модификаторы элементам

--- script.js
+++ script.js
@@ -3,26 +3,24 @@ let editButton = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__name');
 let profileActivity = profile.querySelector('.profile__activity');
 let popup = document.querySelector('.popup');
-let popupCloseButton = popup.querySelector('.popup__close-icon');
-let popupName = popup.querySelector('.popup__name');
-let popupActivity = popup.querySelector('.popup__activity');
-let popupButton = popup.querySelector('.popup__button');
+let popupCloseButton = popup.querySelector('.popup__close-button');
+let popupName = popup.querySelector('.popup__input_type_name');
+let popupActivity = popup.querySelector('.popup__input_type_activity');
+let popupForm = popup.querySelector('.popup__form');
 function openPopup() {
-    popup.setAttribute('style', 'display: block;');
-}
-editButton.addEventListener('click', openPopup);
-function closePopup() {
-    popup.setAttribute('style', 'display: none');
+    popup.classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupActivity.value = profileActivity.textContent;
 }
-popupCloseButton.addEventListener('click', closePopup);
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let profileName = document.querySelector('.profile__name');
-    let profileActivity = document.querySelector('.profile__activity');
     profileName.textContent = `${popupName.value}`;
     profileActivity.textContent = `${popupActivity.value}`;
     closePopup();
 }
-popupButton.addEventListener('click', formSubmitHandler);
+editButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', formSubmitHandler);
